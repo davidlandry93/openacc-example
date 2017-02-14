ac2bde357357ffa8c59dba6e2b4dc0b53eb54daa
David Landry <davidlandry93@gmail.com>
2017-02-14 18:47:26 -0500

    Added managed to the memory

--- gaussian.cc
+++ gaussian.cc
@@ -4,14 +4,12 @@
 #include "gaussian.h"
 
 void gaussian_pdf(Point* points, int n_points, float* result) {
-    Point *pts = points;
-    float *rslt = result;
 
-#pragma acc data present(pts[:n_points]) present(rslt[:n_points])
+#pragma acc data present(points[:n_points]) present(result[:n_points])
 #pragma acc kernels
     {
     for(int i = 0; i < n_points; i++) {
-        rslt[i] = gaussian_pdf_of_point(pts[i]);
+        result[i] = gaussian_pdf_of_point(points[i]);
     }
     }
 }
